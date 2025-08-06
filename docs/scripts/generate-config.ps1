b1a83f394417051ad2ea0d23be7781974f00acdc
後藤晏伴 <95081587+HARUTOGOTO@users.noreply.github.com>
2025-08-06 14:57:03 +0900

    Update generate-config.ps1

--- docs/scripts/generate-config.ps1
+++ docs/scripts/generate-config.ps1
@@ -1,12 +1,6 @@
-# docs/scripts/generate-config.ps1
-
-# Auto-generated. Do not commit.
-$liffIds         = $env:LIFF_IDS
-$univapayAppId   = $env:UNIVAPAY_APP_ID
-$backendEndpoint = $env:BACKEND_ENDPOINT
 @"
-    // Auto-generated config.js
-    export const liffIds = '$liffIds';
-    export const univapayAppId = '$univapayAppId';
-    export const backendEndpoint = '$backendEndpoint';
-"@> ../src/config.js
+// Auto-generated config.js
+export const liffIds         = '$liffIds';
+export const univapayAppId   = '$univapayAppId';
+export const backendEndpoint = '$backendEndpoint';
+"@ > (Join-Path $PSScriptRoot '..\src\config.js')   # ← これで常に docs/src に出力
