6d770abffc82d20401b1b0b6e57127160d3b1e1b
Orihalton <gotouharuto18@gmail.com>
2025-08-24 18:14:55 +0900

    add v2

--- docs/v2/src/config.v2.js
+++ docs/v2/src/config.v2.js
@@ -1,4 +1,4 @@
-export const liffIds            = '["2007034469-N7Zp3pB9"]';                  // ← LIFF IDを配列のJSON文字列で
+export const liffIds            = '["2007034469-N7Zp3pB9,2007034467-z13l7loG"]';                  // ← LIFF IDを配列のJSON文字列で
 export const univapayAppId      = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhcHBfdG9rZW4iLCJpYXQiOjE3NTQ4MTY5NjksIm1lcmNoYW50X2lkIjoiMTFmMDRlYjgtZjJmOC0zMzI0LWJmZTMtNTc1MDQ4MTM2ZjJkIiwic3RvcmVfaWQiOiIxMWYwNGViOC1mNDZhLTc4NDgtOGNiYy01MzUzYjRiMjAzZGMiLCJkb21haW5zIjpbInBheS5vd2FyYS1rYXplLW5vLWJvbi5jb20iXSwibW9kZSI6ImxpdmUiLCJjcmVhdG9yX2lkIjoiMTFmMDRlYjgtZjJmOC0zMzI0LWJmZTMtNTc1MDQ4MTM2ZjJkIiwidmVyc2lvbiI6MSwianRpIjoiMTFmMDc1YzktYjg3MS0xNDE0LWE0OGItYTFkZGU4NWJjMGNlIn0.943371Hs7wXltrudEhhhkYcYUecz44gs_zcmsPSE1sE'; // ← 本番アプリトークン（JWT）<>なし・改行なし
 export const univapayFormId     = '11f07a87-7ef5-c2fc-a45c-db5e7d0ec9f5';   // ← リンクフォームID（/forms/と?appId=の間）
 export const siteBaseUrl        = 'https://pay.owara-kaze-no-bon.com';
